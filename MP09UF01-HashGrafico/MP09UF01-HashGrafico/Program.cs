bef1799ca581e60b01f4f9b6d043613dff0eb28e
RobeEdu2400 <robe_edu@hotmail.com>
2017-09-29 16:40:22 +0200

    Comienzo

---
+++ MP09UF01-HashGrafico/MP09UF01-HashGrafico/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MP09UF01_HashGrafico
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
